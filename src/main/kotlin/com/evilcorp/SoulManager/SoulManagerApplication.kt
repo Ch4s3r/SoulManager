aff325d83e389bcb2468f15861c21d152762ab81
Patrick Lamprecht <lamprecht.patrick1@gmail.com>
2018-05-24 21:39:10 +0200

    init kotlin project

---
+++ src/main/kotlin/com/evilcorp/SoulManager/SoulManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.evilcorp.SoulManager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SoulManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SoulManagerApplication>(*args)
+}
